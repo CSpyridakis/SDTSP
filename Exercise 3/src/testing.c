4e3813410f278e8433e28783d2514f2b131c3f6b
Spyridakis Christos <spyridakischristos@gmail.com>
2020-01-10 22:46:10 +0200

    Exec 3: working with parse

--- Exercise 3/src/testing.c
+++ Exercise 3/src/testing.c
@@ -23,29 +23,84 @@
 // }
 
 
+#define REG_END "end"
+#define REG_CLO "timeToStop"
+#define REG_LIN "^[ ]*[ls|cat]"
 
-int main(int argc, char const *argv[])
-{
-    // if(argc != 2){
+
+#define MORE_THAN_100_CHARS -1
+
+
+int REGEX_SIZE=4096;
+char REGEX[4096];
+
+#define SPACE_NN      "[ ]*"
+#define SPACE_ME      "[ ]+"
+#define END_COMM      "end"
+#define STOP_COMM     "timeToStop"
+#define SIMPLE_COMM   "(ls|cat|cut|tr|grep|" END_COMM "|" STOP_COMM ")"	
+
+// COMMAND parameters
+#define DUAL_QUAT     "(\\\"[^\\\"]*\\\")"
+#define SING_QUAT     "(\\\'[^\\\']*\\\')"
+#define QUATS         "(" DUAL_QUAT "|" SING_QUAT ")"
+#define FLAG          "-[\\\'\\\"]*" QUATS "?"
+
+#define ELSE_PARAM    "([^\\\"\\\']*)"
+
+#define PARAM_TYPE    "(" DUAL_QUAT "|" SING_QUAT "|" FLAG "|" ELSE_PARAM ")"
+#define PARAM         "(" PARAM_TYPE SPACE_ME ")+"
+
+// COMMAND formats
+#define COMMAND_NP    SPACE_NN SIMPLE_COMM SPACE_NN
+#define COMMAND_WP    SPACE_NN SIMPLE_COMM SPACE_ME PARAM
+
+// COMMAND
+#define COMMAND       "(" COMMAND_NP "|" COMMAND_WP ")"
+
+// VALID LINE
+#define VALID_LINE    "^(" COMMAND "|)*" COMMAND "$"
+
+
+int main(int argc, char const *argv[]){
+	printf("^%s$\n", FLAG);	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int lineParse(const char* string){
+	
+// 	if (sizeof(string)>100){
+// 		return MORE_THAN_100_CHARS;
+// 	}
+
+
+
+
+// }
+
+// if(argc != 2){
     //     fprintf(stderr, "Wrong number of arguments!\nPlease run %s -h to see properly usage\n", argv[0]);
     //     exit(EXIT_FAILURE);   
     // }
     // int numOfkids = atoi(argv[1]);
-    char *str = parToVar(argv[1]);
+    // char *str = parToVar(argv[1]);
 
-    printf("Input Line: %s\n", str);
-    findValidCommands(str);
-
-	
-
-
-
-
-
-    
-
-
-
+    // system(str);
     // char delim2[] = "|";
 	// while(ptr != NULL)
 	// {
@@ -60,6 +115,3 @@ int main(int argc, char const *argv[])
 	// 						   the character's ASCII equivalent */
 	// }
 	// printf("\n");
-
-	return 0;
-}
